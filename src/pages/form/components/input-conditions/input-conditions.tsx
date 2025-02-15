116a25593a1aeb16019cf96e0162f85a944e71d7
r1zyh <kirill.ananiev2012@yandex.ru>
2025-02-15 20:43:04 +0300

    Добавит поля для указания локации

--- src/pages/form/components/input-conditions/input-conditions.tsx
+++ src/pages/form/components/input-conditions/input-conditions.tsx
@@ -1,7 +1,41 @@
 export function InputConditions() {
   return (
     <div className="input__container conditions">
-      <h1>Conditions Template</h1>
+      <fieldset className="input__group conditions location">
+        <div className="input__item conditions">
+          <label htmlFor="region" className="input__label">
+            Регион
+          </label>
+          <input
+            type="text"
+            className="input__field conditions"
+            id="region"
+            name="region-name"
+          />
+        </div>
+        <div className="input__item conditions" id="address">
+          <label htmlFor="address" className="input__label">
+            Адрес
+          </label>
+          <input
+            type="text"
+            className="input__field conditions"
+            id="address"
+            name="address-name"
+          />
+        </div>
+        <div className="input__item conditions">
+          <label htmlFor="metro" className="input__label">
+            Станция метро, МЦД
+          </label>
+          <input
+            type="text"
+            className="input__field conditions"
+            id="metro"
+            name="metro-name"
+          />
+        </div>
+      </fieldset>
     </div>
   );
 }
