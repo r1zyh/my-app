47c9085faa5c84f21c1800d0c305638ae3e8e38d
r1zyh <kirill.ananiev2012@yandex.ru>
2025-02-19 00:02:55 +0300

    Исправит неверные размеры контейнера

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -6,27 +6,29 @@ export function Header() {
   const location = useLocation();
   return (
     <header className="header">
-      <nav className="navigation">
-        <div className="white__dot"></div>
-        <div className="navigation__list">
-          <Link
-            className={`navigation__list--item ${
-              location.pathname === AppRoute.Main ? "active" : ""
-            }`}
-            to={AppRoute.Main}
-          >
-            <h2 className="navigation__title">Все заявки</h2>
-          </Link>
-          <Link
-            className={`navigation__list--item ${
-              location.pathname === AppRoute.Form ? "active" : ""
-            }`}
-            to={AppRoute.Form}
-          >
-            <h2 className="navigation__title">Создание заявки</h2>
-          </Link>
-        </div>
-      </nav>
+      <div className="header__container">
+        <nav className="navigation">
+          <div className="white__dot"></div>
+          <div className="navigation__list">
+            <Link
+              className={`navigation__list--item ${
+                location.pathname === AppRoute.Main ? "active" : ""
+              }`}
+              to={AppRoute.Main}
+            >
+              <h2 className="navigation__title">Все заявки</h2>
+            </Link>
+            <Link
+              className={`navigation__list--item ${
+                location.pathname === AppRoute.Form ? "active" : ""
+              }`}
+              to={AppRoute.Form}
+            >
+              <h2 className="navigation__title">Создание заявки</h2>
+            </Link>
+          </div>
+        </nav>
+      </div>
     </header>
   );
 }
