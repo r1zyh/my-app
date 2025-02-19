c3ff0bfec2ecf50e7a481ea9d60baa6b13425c33
r1zyh <kirill.ananiev2012@yandex.ru>
2025-02-19 21:17:41 +0300

    Добавит эффект hover на текстовые и select инпуты

--- src/components/errors/input-with-error.tsx
+++ src/components/errors/input-with-error.tsx
@@ -1,21 +1,25 @@
-interface IInputWithErrorProps extends React.InputHTMLAttributes<HTMLInputElement> {
-    className?: string;
-    error?: string | false;
-  }
-  
-  export const InputWithError = ({
-    className,
-    error,
-    ...props
-  }: IInputWithErrorProps) => {
-    return (
-      <div>
-        <input
-          className={`${className} ${error ? "input__error" : ""}`}
-          {...props}
-        />
-        {error && typeof error === 'string' && <div className="error__message">{error}</div>}
-      </div>
-    );
-  };
-  
+interface IInputWithErrorProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  className?: string;
+  error?: string | false;
+}
+
+export const InputWithError = ({
+  className,
+  error,
+  ...props
+}: IInputWithErrorProps) => {
+  return (
+    <div>
+      <input
+        className={`${className ? className : "with__error"} ${
+          error ? "input__error" : ""
+        }`}
+        {...props}
+      />
+      {error && typeof error === "string" && (
+        <div className="error__message">{error}</div>
+      )}
+    </div>
+  );
+};
