ce7624e1a83f0f5b7e8ade42703cac3e7f3d2c4e
Kirill Ananyev <richeeq@gmail.com>
2025-02-19 23:15:21 +0300

    Тест коммит

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -6,29 +6,27 @@ export function Header() {
   const location = useLocation();
   return (
     <header className="header">
-      <div className="header__container">
-        <nav className="navigation">
-          <div className="white__dot"></div>
-          <div className="navigation__list">
-            <Link
-              className={`navigation__list--item ${
-                location.pathname === AppRoute.Main ? "active" : ""
-              }`}
-              to={AppRoute.Main}
-            >
-              <h2 className="navigation__title">Все заявки</h2>
-            </Link>
-            <Link
-              className={`navigation__list--item ${
-                location.pathname === AppRoute.Form ? "active" : ""
-              }`}
-              to={AppRoute.Form}
-            >
-              <h2 className="navigation__title">Создание заявки</h2>
-            </Link>
-          </div>
-        </nav>
-      </div>
+      <nav className="navigation">
+        <div className="white__dot"></div>
+        <div className="navigation__list">
+          <Link
+            className={`navigation__list--item ${
+              location.pathname === AppRoute.Main ? "active" : ""
+            }`}
+            to={AppRoute.Main}
+          >
+            <h2 className="navigation__title">Все заявки</h2>
+          </Link>
+          <Link
+            className={`navigation__list--item ${
+              location.pathname === AppRoute.Form ? "active" : ""
+            }`}
+            to={AppRoute.Form}
+          >
+            <h2 className="navigation__title">Создание заявки</h2>
+          </Link>
+        </div>
+      </nav>
     </header>
   );
 }
